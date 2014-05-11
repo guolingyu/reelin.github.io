b0ad673a307e86958ac1e477f582d77a919b7305
reelin <reelin.rl@gmail.com>
2014-05-11 16:09:41 +0800

    modify

--- tupu/js/animation.js
+++ tupu/js/animation.js
@@ -1,8 +1,19 @@
 (function() {	
+	
+	var UA = navigator.userAgent,
+		isAndroid = / android|adr/gi.test(UA),
+		isIos = /iphone|ipod/gi.test(UA),
+		animateList = $(".animate-list"),
+		html = '<img class="pic1 active pic" src="images/webhome/wap1.png" alt="图扑"/><img class="pic2 pic" src="images/webhome/wap2.png" alt="图扑"/><img class="pic3 pic" src="images/webhome/wap3.png" alt="图扑"/><img class="pic4 pic" src="images/webhome/wap4.png" alt="图扑"/><img class="pic5 pic" src="images/webhome/wap5.png" alt="图扑"/><img class="pic6 pic" src="images/webhome/wap6.png" alt="图扑"/><img class="pic7 pic" src="images/webhome/wap7.png" alt="图扑"/><img class="pic8 pic" src="images/webhome/wap8.png" alt="图扑"/><img class="pic9 pic" src="images/webhome/wap9.png" alt="图扑"/>';		
+	if (isAndroid || isIos) {
+		animateList.html(html);
+	}
+		// 动画效果
 	var timer = setInterval(function() {
 		var active = $(".animate-list img.active"),
+			last = $(".animate-list img").last(),
 			next;
-		if (active.hasClass("pic3")) {
+		if (active.index() == last.index()) {
 			next = $(".animate-list .pic1");
 		} else {
 			next = active.next()
@@ -11,5 +22,4 @@
 		active.fadeOut(2000).removeClass("active");
 		next.fadeIn(2000).addClass("active");	
 	}, 6000);
-	
 })(jQuery);
