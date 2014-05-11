6a4dcc34bcb73324a1ea2e9d274325cce069f0eb
reelin <reelin.rl@gmail.com>
2014-05-11 16:38:09 +0800

    modify

--- tupu/js/animation.js
+++ tupu/js/animation.js
@@ -5,7 +5,7 @@
 		isIos = /iphone|ipod/gi.test(UA),
 		animateList = $(".animate-list"),
 		html = '<img class="pic1 active pic" src="images/waphome/wap1.png" alt="图扑"/><img class="pic2 pic pic-font" src="images/waphome/wap2.png" alt="图扑"/><img class="pic3 pic" src="images/waphome/wap3.png" alt="图扑"/><img class="pic4 pic pic-font" src="images/waphome/wap4.png" alt="图扑"/><img class="pic5 pic" src="images/waphome/wap5.png" alt="图扑"/><img class="pic6 pic pic-font" src="images/waphome/wap6.png" alt="图扑"/><img class="pic7 pic" src="images/waphome/wap7.png" alt="图扑"/><img class="pic8 pic pic-font" src="images/waphome/wap8.png" alt="图扑"/><img class="pic9 pic" src="images/waphome/wap9.png" alt="图扑"/>';		
-	// if (isAndroid || isIos) {
+	if (isAndroid || isIos) {
 		animateList.html(html);
 		var timer = setInterval(function() {
 			var active = $(".animate-list img.active"),
@@ -25,21 +25,21 @@
 			});
 			
 		}, 5000);
-	// } else {
-	// 	// 动画效果
-	// 	var timer = setInterval(function() {
-	// 		var active = $(".animate-list img.active"),
-	// 			last = $(".animate-list img").last(),
-	// 			next;
-	// 		if (active.index() == last.index()) {
-	// 			next = $(".animate-list .pic1");
-	// 		} else {
-	// 			next = active.next()
-	// 		}
+	} else {
+		// 动画效果
+		var timer = setInterval(function() {
+			var active = $(".animate-list img.active"),
+				last = $(".animate-list img").last(),
+				next;
+			if (active.index() == last.index()) {
+				next = $(".animate-list .pic1");
+			} else {
+				next = active.next()
+			}
 			
-	// 		active.fadeOut(2000).removeClass("active");
-	// 		next.fadeIn(2000).addClass("active");	
-	// 	}, 6000);
-	// }
+			active.fadeOut(2000).removeClass("active");
+			next.fadeIn(2000).addClass("active");	
+		}, 6000);
+	}
 	
 })(jQuery);
