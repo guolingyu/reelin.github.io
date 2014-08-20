0d5152693440b871d37667d6d3f4b39f3ddb5268
reelin <reelin.rl@gmail.com>
2014-08-20 12:03:20 +0800

    添加demo

--- jian/javascript/animation.js
+++ jian/javascript/animation.js
@@ -7,11 +7,26 @@
         wheelHeight = wheel.height(),
         adventure = $("#adventure"),
         advWidth = adventure.width(),
-        advHeight = adventure.height();
-    $(".main-building").css("visibility", "hidden");
+        advHeight = adventure.height(),
+        icon = $(".j-icon"),
+        iconTopList = [];
+
+    // 初始动画元素状态
+    $(".main-building, .j-icon").css("visibility", "hidden");
+    icon.each(function() {
+        var that = $(this);
+        that.data("top", that.css("top"));
+        that.css("top", "-200px");
+    });
+
+    // 调用动画函数
     $(window).load(function() {
         carouselAnimation();
     });
+
+    /**
+     * 动画效果
+     */
     function carouselAnimation() {
         carousel.animate({
             height: 0,
@@ -76,9 +91,21 @@
                         width: advWidth,
                         height: advHeight,
                         top: -15
-                    }, 300);
+                    }, 300, function() {
+                        dropIcons();
+                    });
                 }
             });
+        });
+    }
+    function dropIcons() {
+        icon.css("visibility", "visible");
+        icon.each(function() {
+            var that = $(this),
+                top = that.data("top");
+            that.animate({
+                top: top
+            }, 500);
         });
     }
 })();
