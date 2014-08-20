b80eba88bc849638428d4fff630db5cd4b955aca
reelin <reelin.rl@gmail.com>
2014-08-20 11:42:40 +0800

    添加demo

---
+++ jian/javascript/animation.js
@@ -0,0 +1,84 @@
+(function() {
+    var carousel = $("#carousel"),
+        carWidth = carousel.width(),
+        carHeight = carousel.height(),
+        wheel = $("#wheel"),
+        wheelWidth = wheel.width(),
+        wheelHeight = wheel.height(),
+        adventure = $("#adventure"),
+        advWidth = adventure.width(),
+        advHeight = adventure.height();
+
+    carouselAnimation();
+    function carouselAnimation() {
+        carousel.animate({
+            height: 0,
+            top: 400
+        }, 10, function() {
+            carousel.css("visibility", "visible");
+            carousel.animate({
+                width: carWidth*1.2,
+                height: carHeight*1.2,
+                top: 100
+            }, {
+                duration: 800,
+                complete: function() {
+                    wheel.animate({
+                        width: wheelWidth,
+                        height: wheelHeight,
+                        top: "160px"
+                    }, 300, function() {
+                        wheelAnimation();
+                    });
+                }
+            });
+        });
+    }
+
+    function wheelAnimation() {
+        wheel.animate({
+            height: 0,
+            top: 200
+        }, 10, function() {
+            wheel.css("visibility", "visible");
+            wheel.animate({
+                width: wheelWidth*1.2,
+                height: wheelHeight*1.2,
+                top: -170
+            }, {
+                duration: 800,
+                complete: function() {
+                    wheel.animate({
+                        width: wheelWidth,
+                        height: wheelHeight,
+                        top: -103
+                    }, 300, function() {
+                        adventureAnimation();
+                    });
+                }
+            });
+        });
+    }
+    function adventureAnimation() {
+        adventure.animate({
+            height: 0,
+            top: 345
+        }, 10, function() {
+            adventure.css("visibility", "visible");
+            adventure.animate({
+                width: advWidth*1.2,
+                height: advHeight*1.2,
+                top: -90
+            }, {
+                duration: 800,
+                complete: function() {
+                    adventure.animate({
+                        width: advWidth,
+                        height: advHeight,
+                        top: -15
+                    }, 300);
+                }
+            });
+        });
+    }
+})();
