e62ca836ec965ee3bcc5d348d05df09a161c84fa
reelin <reelin.rl@gmail.com>
2014-11-22 17:52:57 +0800

    modify

--- tupu/js/skip.js
+++ tupu/js/skip.js
@@ -1,4 +1,4 @@
-(function() {
+function skip(link) {
     function localParam(search,hash){
         search = search || window.location.search;
         hash = hash || window.location.hash;
@@ -23,17 +23,16 @@
         autoopen = params.search['autoopen']||params.hash['autoopen'],
         isappinstalled = params.search['isappinstalled'],
         appinstall = params.search['appinstall'],
-        qqLink = 'http://reelin.github.io/tupu/qq.html',
-        wxLink = 'http://reelin.github.io/tupu/weixin.html',
+        qqLink = 'http://tupu.im/static/qq.html',
+        wxLink = 'http://tupu.im/static/weixin.html',
         iDownload = 'itms-apps://itunes.apple.com/us/app/tu-pu-yong-zhao-pian-chuang/id915233164?ls=1&amp;mt=8',
-        openAppLink = 'newsapp://',
-        skipAppLink = 'newsapp://web/http://c.m.163.com/CreditMarket/default.html',
-        iframe = document.getElementById('ifr');
+        skipAppLink = link,
+        iframe = jQuery('#ifr');
 
     if( (isIDevice || isIDeviceIpad) && !isAndroid){
-        openAppLink = openAppLink || 'newsapp://';
+        skipAppLink = skipAppLink || 'tupu://';
     }else if(isAndroid){
-        openAppLink = 'http://tupu.im/';
+        skipAppLink= 'http://tupu.im/';
     }
     if(isappinstalled!==undefined){
         wxLink += '?isappinstalled='+isappinstalled+'&openurl='+openAppLink;
@@ -45,49 +44,14 @@
         wxLink += '?openurl='+openAppLink;
         qqLink += '?openurl='+openAppLink;
     }
-    alert(qqLink);
 
     function download(){
-        //  if(isAndroid){
         window.location = 'http://tupu.im/';
-//            }else{
-//                window.location = iDownload;
-//            }
     }
-    /*
-     * iOS点击打开:
-     1.如果是微信就去引导图页面
-     2.如果不是微信就走安装就打开不安装就去app store
-     3.如果微信用户按引导图从浏览器打开就能走通第2条
-
-     android点击打开:
-     1.如果是微信就在打开的时候同时跳转到有图的引导页
-     2.如果不是微信就同时跳转到公公共下载页
-     3.如果微信用户按引导图从浏览器打开就能走通第2条
-     */
-    function open(){
-        if(isWeixin){
-            window.location = wxLink;
-        }else if((isIDevice || isIDeviceIpad) && !isAndroid){
-            if(isQQ) {
-                alert(qqLink);
-                window.location = qqLink;
-            } else {
-                window.location = openAppLink;
-                setTimeout(function(){
-                    window.location = iDownload;
-                }, 50);
-            }
-        } else {
-            iframe.src = openAppLink;
-            download();
-        }
-    }
-    function skip() {
+    function open() {
         if(isWeixin){
             window.location = wxLink;
         }else if(isQQ) {
-            alert(qqLink);
             window.location = qqLink;
         }else if((isIDevice || isIDeviceIpad) && !isAndroid){
             window.location = skipAppLink;
@@ -101,14 +65,10 @@
     }
     $(".install-small, .footer .install").on("click", function(e) {
         e.preventDefault();
-        open();
+        download();
     });
     $(".modal .install").on("click", function(e) {
         e.preventDefault();
-        skip();
-    });
-    //自动打开
-    if(autoopen==1){
         open();
-    }
-})();
+    });
+};
