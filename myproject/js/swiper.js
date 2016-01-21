f9a15043ec60230c869949a0aa7605f4c4ed65b1
reelin <reelin.rl@gmail.com>
2016-01-21 21:43:24 +0800

    modify

--- myproject/js/swiper.js
+++ myproject/js/swiper.js
@@ -1,30 +1,36 @@
 (function($){
 	$('.thumb .img-wrapper').hammer().on("swipeleft", function() {
 		var that = $(this);
-		var imgList = $(this).find('img');
+		var imgList = that.find('img');
 		var imgListLen = imgList.length;
-		var imgWrapper = $(this).find('div');
+		var imgWrapper = that.find('div');
 		var dis = 580;
 		var maxLeft = dis * (imgListLen - 1); 
+		var currentPage = that.find('.js-current-page');
 		var left = parseInt(imgWrapper.css('margin-left'));
-    	if (imgListLen > 1 && left < maxLeft) {
+
+    	if (imgListLen > 1 && left > - maxLeft) {
     		imgWrapper.animate({
     			'margin-left': left - dis
-    		}, 500);
+    		}, 500, function() {
+    			currentPage.html(parseInt(currentPage.html()) + 1);
+    		});
     	}
 	}).on('swiperight', function() {
 		var that = $(this);
-		var imgList = $(this).find('img');
+		var imgList = that.find('img');
 		var imgListLen = imgList.length;
-		var imgWrapper = $(this).find('div');
+		var imgWrapper = that.find('div');
 		var dis = 580;
-		var maxLeft = dis * (imgListLen - 1); 
 		var left = parseInt(imgWrapper.css('margin-left'));
-    	var left = parseInt(imgWrapper.css('margin-left'));
-    	if (imgListLen > 1 && left > 0) {
+    	var currentPage = that.find('.js-current-page');
+		
+    	if (imgListLen > 1 && left < 0) {
     		imgWrapper.animate({
     			'margin-left': left + dis
-    		}, 500);
+    		}, 500, function() {
+    			currentPage.html(parseInt(currentPage.html()) - 1);
+    		});
     	} 
     });
 
